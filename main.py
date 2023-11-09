d4cc5531060d511e03b036794448628645016ee2
alexxten <alexxten13@gmail.com>
2023-11-09 21:38:54 +0300

    endpoints init

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from enum import Enum
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List, Optional, Literal
+from fastapi import Depends
 
 app = FastAPI()
 
 
 class DogType(str, Enum):
-    terrier = "terrier"
-    bulldog = "bulldog"
-    dalmatian = "dalmatian"
+    terrier = 'terrier'
+    bulldog = 'bulldog'
+    dalmatian = 'dalmatian'
 
 
 class Dog(BaseModel):
@@ -24,7 +26,7 @@ class Timestamp(BaseModel):
 
 dogs_db = {
     0: Dog(name='Bob', pk=0, kind='terrier'),
-    1: Dog(name='Marli', pk=1, kind="bulldog"),
+    1: Dog(name='Marli', pk=1, kind='bulldog'),
     2: Dog(name='Snoopy', pk=2, kind='dalmatian'),
     3: Dog(name='Rex', pk=3, kind='dalmatian'),
     4: Dog(name='Pongo', pk=4, kind='dalmatian'),
@@ -40,8 +42,36 @@ post_db = [
 
 @app.get('/')
 def root():
-    # ваш код здесь
-    ...
+    return {
+        'message': (
+            'Welcome, это сервис на FastApi, '
+            'возвращающий информацию по собакам для ветклиники'
+        ),
+    }
 
-# ваш код здесь
-...
+
+@app.post('/post')
+def get_post() -> Timestamp:
+    return Timestamp()
+
+
+@app.get('/dog')
+def get_dogs(
+        kind: DogType | None = None,
+) -> List[Dog]:
+    return [Dog()]
+
+
+@app.post('/dog')
+def create_dog(dog: Dog) -> Dog:
+    return dog
+
+
+@app.get('/dog/{pk}')
+def get_dog_by_pk(pk: int) -> Dog:
+    return Dog()
+
+
+@app.patch('/dog/{pk}')
+def update_dog_by_pk(pk: int) -> Dog:
+    return Dog()
